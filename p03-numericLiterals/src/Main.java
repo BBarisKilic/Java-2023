90e57f1234bf8935674550d19eea39afca687c52
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2023-10-13 00:14:36 +0200

    feat: add numericLiterals

---
+++ p03-numericLiterals/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 2_000_000_000;
+        double transfer = 500_000_000.01;
+        double total = amount + transfer;
+        System.out.println(total);
+    }
+}
