d8b0507d02de2c8f0cccbb0e2e60b1d7a8e8eeb3
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2023-10-12 23:57:50 +0200

    feat: add primitiveTypes

---
+++ p02-primitiveTypes/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int number1 = 20;
+        int number2 = 30;
+        int sum = number1 + number2;
+        System.out.println("The sum of " + number1 + " and " + number2 + " is " + sum);
+
+        double pi = 3.14;
+        System.out.println("The value of pi is " + pi);
+
+        boolean isJavaFun = true;
+        System.out.println("Is Java fun? " + isJavaFun);
+
+        char myGrade = 'A';
+        System.out.println("My grade is " + myGrade);
+    }
+}
