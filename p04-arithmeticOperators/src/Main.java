ea8cea70b460f021f238701888c29889ef5ded6a
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2023-10-13 00:18:24 +0200

    feat: add arithmeticOperators

---
+++ p04-arithmeticOperators/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int numberOne = 10;
+        int numberTwo = 3;
+        System.out.println(numberOne + numberTwo);
+        System.out.println(numberOne - numberTwo);
+        System.out.println(numberOne * numberTwo);
+        System.out.println(numberOne / numberTwo);
+        System.out.println(numberOne % numberTwo);
+    }
+}
