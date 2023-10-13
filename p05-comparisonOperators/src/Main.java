6972ffd394e8e87161a60dd8df5329590b048ea8
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2023-10-13 20:02:20 +0200

    feat: add comparisonOperators

---
+++ p05-comparisonOperators/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // Comparison Operators
+        // ==, !=, >, <, >=, <=
+        System.out.println(1 == 1); // true
+        System.out.println(1 != 1); // false
+        System.out.println(1 > 1); // false
+        System.out.println(1 < 1); // false
+        System.out.println(1 >= 1); // true
+        System.out.println(1 <= 1); // true
+    }
+}
