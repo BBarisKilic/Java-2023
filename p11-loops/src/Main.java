d7e254d67ea378b24af480dfdcdb3d95d6fd2536
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2023-10-13 23:01:10 +0200

    feat: add loops

---
+++ p11-loops/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // Loops
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+
+        // Reverse for loop
+        for (int i = 10; i > 0; i--) {
+            System.out.println(i);
+        }
+    }
+}
