fe185858211d8ef22d4ca6a2f8e63eea5109dbb3
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2023-10-13 23:26:08 +0200

    feat: add ifStatements

---
+++ p13-ifStatements/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // If statement
+        if (true) {
+            System.out.println("This code will always run");
+        } else {
+            System.out.println("This code will never run");
+        }
+
+        System.out.println("Outside of if statement");
+    }
+}
