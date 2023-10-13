34eabcc87f8c55335b296bee933d48bcf9b5eb5c
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2023-10-13 20:42:33 +0200

    feat: add namingVariables

---
+++ p07-namingVariables/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // Bad naming convention
+        int Goals = 3;
+        int Assists = 2;
+        System.out.println(Goals + Assists);
+
+        // Good naming convention
+        int goals = 3;
+        int numberOfAssists = 2;
+        System.out.println(goals + numberOfAssists);
+    }
+}
