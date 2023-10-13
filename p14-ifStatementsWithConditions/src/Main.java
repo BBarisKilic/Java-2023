8ae487f0bf5b1a4e12f7dbd53c08134b1a9f69ab
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2023-10-13 23:30:45 +0200

    feat: add ifStatementsWithConditions

---
+++ p14-ifStatementsWithConditions/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // If statement with condition
+        int age = 15;
+        boolean isAdult = age >= 18;
+
+        if (isAdult) {
+            System.out.println("You are an adult");
+        } else {
+            System.out.println("You are not an adult");
+        }
+    }
+}
