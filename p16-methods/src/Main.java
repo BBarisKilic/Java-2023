8675d9e1d99931f15b997a19bd74925554cb0f95
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2023-10-13 23:55:20 +0200

    feat: add methods

---
+++ p16-methods/src/Main.java
@@ -0,0 +1,34 @@
+public class Main {
+    /*
+        - Access Modifiers
+            - public
+            - private
+            - protected
+            - default
+        - Return Type
+            - void
+            - int
+            - String
+            - double
+            - float
+            - char
+            - boolean
+            - byte
+            - short
+            - long
+            - Object
+            - Array
+            - Class
+            - Interface
+            - Enum
+            - Annotation
+            - void
+         - Name
+         - Optional Parameters
+         - Body
+         - Optional return
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
