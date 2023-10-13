d9d40867af4efab11536a9142c6cd71c14115344
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2023-10-13 20:18:25 +0200

    feat: add shortHandReassignment

---
+++ p06-shortHandReassignment/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int numberOne = 10;
+        int numberTwo = 20;
+
+        numberTwo += 2;
+        System.out.println(numberTwo);
+
+        numberOne *= numberTwo;
+        System.out.println(numberOne);
+    }
+}
