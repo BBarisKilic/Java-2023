7b2d35175072b96d018e9352279033d0db167072
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2023-10-21 00:11:31 +0200

    feat: create customer entity

---
+++ p21-nTierArchitecture/src/main/java/com/example/p21ntierarchitecture/P21NTierArchitectureApplication.java
@@ -0,0 +1,13 @@
+package com.example.p21ntierarchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P21NTierArchitectureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(P21NTierArchitectureApplication.class, args);
+    }
+
+}
