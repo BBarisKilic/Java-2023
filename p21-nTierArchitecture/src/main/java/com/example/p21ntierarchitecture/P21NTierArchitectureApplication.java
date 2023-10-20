13a8bddb6e479e1e17650e3b5b5e3b4afe06d284
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2023-10-21 00:33:18 +0200

    feat: add read customer api

--- p21-nTierArchitecture/src/main/java/com/example/p21ntierarchitecture/P21NTierArchitectureApplication.java
+++ p21-nTierArchitecture/src/main/java/com/example/p21ntierarchitecture/P21NTierArchitectureApplication.java
@@ -2,12 +2,29 @@ package com.example.p21ntierarchitecture;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/api/v1/customers")
 public class P21NTierArchitectureApplication {
+
+    private final CustomerRepository customerRepository;
+
+    public P21NTierArchitectureApplication(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(P21NTierArchitectureApplication.class, args);
     }
 
+    @GetMapping
+    public List<Customer> getCustomers() {
+        return customerRepository.findAll();
+    }
 }
