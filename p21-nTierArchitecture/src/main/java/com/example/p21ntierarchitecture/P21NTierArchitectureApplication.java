e9ce39564b3f59aa89f75fefd4b91d80d61997b9
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2023-10-23 20:48:30 +0200

    feat: add save customer api

--- p21-nTierArchitecture/src/main/java/com/example/p21ntierarchitecture/P21NTierArchitectureApplication.java
+++ p21-nTierArchitecture/src/main/java/com/example/p21ntierarchitecture/P21NTierArchitectureApplication.java
@@ -3,6 +3,7 @@ package com.example.p21ntierarchitecture;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,5 +27,19 @@ public class P21NTierArchitectureApplication {
     @GetMapping
     public List<Customer> getCustomers() {
         return customerRepository.findAll();
+    }
+
+
+    public record NewCustomerRequest(String name, String email, Integer age) {}
+
+    @RequestMapping
+    public void addCustomer(@RequestBody NewCustomerRequest newCustomerRequest) {
+        Customer customer = new Customer();
+
+        customer.setName(newCustomerRequest.name());
+        customer.setEmail(newCustomerRequest.email());
+        customer.setAge(newCustomerRequest.age());
+
+        customerRepository.save(customer);
     }
 }
